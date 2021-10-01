cebd695a79eb09c1414c141d81c06b8aaa50b781
proneodev <obidjuraev07@gmail.com>
2021-10-01 19:59:22 +0500

    category model added and fixed

--- ad/models.py
+++ ad/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50,  null=False, blank=False)
+    parent = models.ForeignKey('self', related_name='father', on_delete=models.CASCADE, blank=True, null=True)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        if self.parent and self.parent.name == self.name:
+            raise ValidationError('Xato....')
+        else:
+            if not self.slug:
+                self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+    def str(self):
+        return f'{self.name}'
