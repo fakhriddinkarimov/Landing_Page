e2abf809ea3b4d317ce9a8523652a7c7b1b8fc03
Lazizbek | ROOT_RU <user@Windows.naw.deect.ru>
2021-10-01 20:31:01 +0500

    added templates and app registration

---
+++ registration/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class UserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username','email','password1','password2']
+		widgets = {
+		'username': forms.TextInput(attrs={'placeholder': 'Username','class':'form-control'}),
+		'email': forms.TextInput(attrs={'placeholder': 'E-mail','class':'form-control'}),
+		'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+		'password2': forms.TextInput(attrs={'placeholder': 'Retype password','class':'form-control'}),
+		}
+
+	def save(self,commit=True):
+		user = super().save(commit=False)
+		if commit:
+			user.save()
+		return user
