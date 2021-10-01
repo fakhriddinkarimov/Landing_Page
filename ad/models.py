87d8cda12818344a6f0d4494d2c2618d58459a95
proneodev <obidjuraev07@gmail.com>
2021-10-01 22:48:13 +0500

    create product  model

--- ad/models.py
+++ ad/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -18,3 +19,21 @@ class Category(models.Model):
 
     def str(self):
         return f'{self.name}'
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='media/product/image/')
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    decription = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class ProductImage(models.Model):
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
