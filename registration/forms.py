fd57fcf01d060da17deb6474210df0c68a040746
Lazizbek | ROOT_RU <1.cocoshine@gmail.com>
2021-10-05 12:44:59 +0500

    Templates & Registration were modified

--- registration/forms.py
+++ registration/forms.py
@@ -5,9 +5,11 @@ from django import forms
 class UserForm(UserCreationForm):
 	class Meta:
 		model = User
-		fields = ['username','email','password1','password2']
+		fields = ['username','first_name','last_name','email','password1','password2']
 		widgets = {
 		'username': forms.TextInput(attrs={'placeholder': 'Username','class':'form-control'}),
+		'first_name': forms.TextInput(attrs={'placeholder': 'First Name','class':'form-control'}),
+		'last_name': forms.TextInput(attrs={'placeholder': 'Last Name','class':'form-control'}),
 		'email': forms.TextInput(attrs={'placeholder': 'E-mail','class':'form-control'}),
 		'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
 		'password2': forms.TextInput(attrs={'placeholder': 'Retype password','class':'form-control'}),
