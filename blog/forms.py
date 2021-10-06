abd23b628cd432fc5f9495ae0a5f5b794190b323
shahbozdeveloper <germanationit11@gmail.com>
2021-10-06 20:23:00 +0500

    blog modified

--- blog/forms.py
+++ blog/forms.py
@@ -1,19 +1,23 @@
 from django import forms
-from .models import Blog, Comments
+from .models import Blog,Comments
 
 
 class BlogModelForm(forms.ModelForm):
+    name = forms.CharField(max_length=200,widget=forms.TextInput(attrs={"class" : "form-control",
+                                                                        'placeholder': "Name"}))
+    description = forms.CharField(widget=forms.Textarea(attrs={"class" : "form-control",
+                                                                           'placeholder': "Description"}))
+
     class Meta:
         model = Blog
-        fields = ['name', 'description', 'image']
-
-class BlogForm(forms.Form):
-    class Meta:
-        model = Blog
-        fields = '__all__'
+        fields = ['name', 'description', 'image', 'category']
 
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model=Comments
-        fields='__all__'
+    def save(self, commit=True):
+        self.instance.created_date
+        super().save()
+        return self.instance
+
+
+class Comment(forms.Form):
+    message = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control","id":"comment-message"}))
--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,13 @@ from django.utils.text import slugify
 from tinymce import models as tinymce_models
 from ad.models import Category
 from django.contrib.auth.models import User
+
 class Blog(models.Model):
     name = models.CharField(max_length=255)
-    description = tinymce_models.HTMLField()
+    description =tinymce_models.HTMLField()
     slug = models.SlugField(max_length=255)
-    image = models.ImageField(upload_to='static/img/blog/')
-    created_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='static/img/')
+    created_date = models.DateField(auto_now_add=True)
     category = models.ForeignKey(
         Category, on_delete =models.CASCADE
     )
@@ -16,7 +17,7 @@ class Blog(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
-        super(Blog, self).save(*args, **kwargs)
+            super(Blog, self).save(*args, **kwargs)
 
 
     def __str__(self):
